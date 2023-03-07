6bdbd5490c53991a57aa5011f37345ef3ec613df
KwamesKorner <kwame.asante@kxtgconsulting.com>
2023-03-06 23:35:51 -0500

    move permission

--- script.js
+++ script.js
@@ -31,6 +31,22 @@ function begin() {
     var startButton = document.getElementById("start-button");
     startButton.classList.toggle("hide")
 
+    if (typeof DeviceMotionEvent.requestPermission === 'function') {
+        // Handle iOS 13+ devices.
+        DeviceMotionEvent.requestPermission()
+          .then((state) => {
+            if (state === 'granted') {
+              window.addEventListener('deviceorientation', handleOrientation);
+            } else {
+              console.error('Request to access the orientation was rejected');
+            }
+          })
+          .catch(console.error);
+    } else {
+        // Handle regular non iOS 13+ devices.
+        window.addEventListener('devicemotion', handleOrientation);
+    }
+
     // Set the initial countdown value
     var countDownValue = 3;
 
@@ -52,21 +68,6 @@ function begin() {
             // document.getElementById("countdown").innerHTML = "GO!";
             countdown.classList.toggle("show")
             next()
-            if (typeof DeviceMotionEvent.requestPermission === 'function') {
-                // Handle iOS 13+ devices.
-                DeviceMotionEvent.requestPermission()
-                  .then((state) => {
-                    if (state === 'granted') {
-                      window.addEventListener('deviceorientation', handleOrientation);
-                    } else {
-                      console.error('Request to access the orientation was rejected');
-                    }
-                  })
-                  .catch(console.error);
-            } else {
-            // Handle regular non iOS 13+ devices.
-            window.addEventListener('devicemotion', handleOrientation);
-            }
         }
     }, 1000);
 
