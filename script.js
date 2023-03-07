97c4717e98d71689854170f5aecced92568032e8
KwamesKorner <kwame.asante@kxtgconsulting.com>
2023-03-06 23:31:26 -0500

    add pause after function call

--- script.js
+++ script.js
@@ -143,16 +143,16 @@ function handleOrientation(event) {
     // document.querySelector("#gamma").innerHTML = gamma;
 
     if((30 <= gamma && gamma <= 60) || (-60 <= gamma && gamma <= -30)) {
+        next();
         setTimeout(() => {
-            next();
-          }, 1500);
+        }, 1500);
 
     }
 
     else if((10 <= gamma && gamma <= 40) || (-40 <= gamma && gamma <= -10)) {
+        next();
         setTimeout(() => {
-            next();
-          }, 1500);
+        }, 1500);
     }
 }
 
